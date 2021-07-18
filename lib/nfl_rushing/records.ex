d3e7cb0fd06fefa90a498dd8ea555c6b26dc04b1
Raymond Chang <rchang21@gmail.com>
2021-07-17 21:50:24 -0700

    Added tailwind. Implemented sorting for 3 columns and filtering

--- lib/nfl_rushing/records.ex
+++ lib/nfl_rushing/records.ex
@@ -3,10 +3,19 @@ defmodule NflRushing.Records do
   The Records context for a player's record of rushing statistics
   """
 
+  import Ecto.Query
   alias NflRushing.Repo
   alias NflRushing.Record
 
   def list_records() do
     Repo.all(Record)
+  end
+
+  def list_records_with_player(name) do
+    name_wildcard = "%" <> name <> "%"
+
+    Record
+    |> where([r], ilike(r.player, ^name_wildcard))
+    |> Repo.all()
   end
 end
